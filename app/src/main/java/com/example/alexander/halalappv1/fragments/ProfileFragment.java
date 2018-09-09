c5e15c1d52fbb33ab7f95e3c245a0c6ca19c0ffd
hazem ashraf <hazem_ashraf126@yahoo.com>
2018-09-09 21:53:47 +0200

    modify profile screen

--- app/src/main/java/com/example/alexander/halalappv1/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/alexander/halalappv1/fragments/ProfileFragment.java
@@ -5,24 +5,40 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.alexander.halalappv1.R;
 import com.example.alexander.halalappv1.activities.FavouriteRestaurantsActivity;
 import com.example.alexander.halalappv1.activities.ProfileSettingsActivity;
+import com.example.alexander.halalappv1.adapters.HomeRestaurantAdapter;
+import com.example.alexander.halalappv1.model.modifiedmodels.Restaurant;
+import com.example.alexander.halalappv1.services.RetrofitWebService;
 import com.example.alexander.halalappv1.utils.ConstantsHelper;
 import com.example.alexander.halalappv1.utils.SharedPreferencesHelper;
 
-public class ProfileFragment extends Fragment {
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ProfileFragment extends Fragment implements HomeRestaurantAdapter.OnRestaurantClickListener{
 
     public ProfileFragment() {}
 
     private TextView mUserNameTextView;
     private ConstraintLayout mUserNameLayout;
     private ConstraintLayout mFavouriteRestaurantsLayout;
+    private RecyclerView mFavouriteRecyclerView;
+    private ArrayList<Restaurant> mFavResList;
+    private HomeRestaurantAdapter mRestaurantAdapter;
+
     //==============================================================================================
     private void updateUserNameView() {
         boolean isLoggedIn = SharedPreferencesHelper.getSharedPreferenceBoolean(getContext(), ConstantsHelper.KEY_IS_LOGGED_IN, false);
@@ -66,6 +82,7 @@ public class ProfileFragment extends Fragment {
         mUserNameTextView = rootView.findViewById(R.id.tv_profile_fragment_user_name);
         mUserNameLayout = rootView.findViewById(R.id.profile_fragment_user_name_layout);
         mFavouriteRestaurantsLayout = rootView.findViewById(R.id.profile_fragment_favourite_restaurants_layout);
+        mFavouriteRecyclerView = rootView.findViewById(R.id.myFavRecyclerView);
 
         updateUserNameView();
 
@@ -73,6 +90,37 @@ public class ProfileFragment extends Fragment {
 
         favouriteRestaurantsLayoutClick();
 
+        getFavRestaurents();
         return rootView;
+    }
+
+    private void getFavRestaurents(){
+        int userId = SharedPreferencesHelper.getSharedPreferenceInt(getContext(), ConstantsHelper.KEY_USER_ID, 0);
+        RetrofitWebService webService = RetrofitWebService.retrofit.create(RetrofitWebService.class);
+        Call<ArrayList<Restaurant>> favRestaurents = webService.getFavouriteRestaurants(userId);
+        favRestaurents.enqueue(new Callback<ArrayList<Restaurant>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Restaurant>> call, Response<ArrayList<Restaurant>> response) {
+                mFavResList = response.body();
+                mFavouriteRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayout.HORIZONTAL, false));
+                mRestaurantAdapter = new HomeRestaurantAdapter(getContext(), ProfileFragment.this);
+                mRestaurantAdapter.setRestaurantList(mFavResList);
+                mRestaurantAdapter.setTablePosition(0);
+                mFavouriteRecyclerView.setAdapter(mRestaurantAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Restaurant>> call, Throwable t) {
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onRestaurantClick(int parentPosition, int childPosition) {
+
     }
 }
