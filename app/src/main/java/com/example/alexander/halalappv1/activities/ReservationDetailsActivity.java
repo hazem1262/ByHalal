4483b2e4927bc765e0f1537c387c1bd6a97881a4
daniel <daniel.raouf@innuva.com>
2018-10-27 20:55:00 +0400

    add reservation details activity

---
+++ app/src/main/java/com/example/alexander/halalappv1/activities/ReservationDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexander.halalappv1.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_details);
+    }
+}
